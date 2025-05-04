f9373a41485e489327c0b70ee9b414e46c91c76a
fatemearyaie <fatemearyaie9@gmail.com>
2025-05-04 18:26:15 +0330

    edit admin.py to display more data field in admin panel

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import article
 
+class articleadmin(admin.ModelAdmin):
+    list_display = ('title','slug','publish','status')
 # Register your models here.
-admin.site.register(article)
+admin.site.register(article, articleadmin)
+
+    
