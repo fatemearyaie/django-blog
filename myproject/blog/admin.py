2855142f978791cb0a12b981504fb8d53a4e171f
fatemearyaie <fatemearyaie9@gmail.com>
2025-05-04 18:28:06 +0330

    add filter in admin panel

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -3,6 +3,7 @@ from .models import article
 
 class articleadmin(admin.ModelAdmin):
     list_display = ('title','slug','publish','status')
+    list_filter = ('publish', 'status')
 # Register your models here.
 admin.site.register(article, articleadmin)
 
