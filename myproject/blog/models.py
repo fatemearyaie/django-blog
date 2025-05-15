e2d963b183c99b589a04c44d57f06c80f2fbb3aa
fatemearyaie <fatemearyaie9@gmail.com>
2025-05-15 20:06:47 +0330

    edit each field name in admin panel

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -7,14 +7,14 @@ class article(models.Model):
         ('D', 'Draft'),
         ('P', 'Published')
     )
-    title = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=100, unique=True) #اون بخشی از url که تغییر میکنه رو میگیم اسلاگ 
-    description = models.TextField()
-    thumbnail = models.ImageField(upload_to='images')
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    upldated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    title = models.CharField(max_length=50, verbose_name="عنوان")
+    slug = models.SlugField(max_length=100, unique=True, verbose_name="اسلاگ") #اون بخشی از url که تغییر میکنه رو میگیم اسلاگ 
+    description = models.TextField(verbose_name="توضیحات")
+    thumbnail = models.ImageField(upload_to='images', verbose_name="عکس بندانگشتی")
+    publish = models.DateTimeField(default=timezone.now, verbose_name="تاریخ انتشار")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ساخت")
+    updated = models.DateTimeField(auto_now=True, verbose_name="تاریخ آپدیت")
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, verbose_name="وضعیت")
 
     class Meta:
         verbose_name = "مقاله"
