9d8beffd349e8ca8abe37823ee07126ba0c95bee
fatemearyaie <fatemearyaie9@gmail.com>
2025-05-17 17:10:11 +0330

    write category_to_str def to show the category field in article table, use jpublish instead of publish to have jalali date in tables.

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,19 +1,6 @@
 from django.contrib import admin
 from .models import article, category
 
-class articleadmin(admin.ModelAdmin):
-    list_display = ('title','slug','publish','status',)
-    list_filter = ('publish', 'status')
-    search_fields = ('title', 'slug')
-    # generate slug field for every title automatically.
-    prepopulated_fields = {'slug':['title']}
-    ordering = ['status', 'publish']
-
-    
-# Register your models here.
-admin.site.register(article, articleadmin)
-
-    
 class categoryadmin(admin.ModelAdmin):
     list_display = ('title','slug','status', 'position')
     list_filter = [('status')]
@@ -24,5 +11,24 @@ class categoryadmin(admin.ModelAdmin):
     
 # Register your models here.
 admin.site.register(category, categoryadmin)
+
+
+class articleadmin(admin.ModelAdmin):
+    list_display = ('title','slug','jpublish','status', 'category_to_str')
+    list_filter = ('publish', 'status')
+    search_fields = ('title', 'slug')
+    # generate slug field for every title automatically.
+    prepopulated_fields = {'slug':['title']}
+    ordering = ['status', 'publish']
+
+    def category_to_str(self, obj):
+        return "categories"
+
+    
+# Register your models here.
+admin.site.register(article, articleadmin)
+
+    
+
 
     
