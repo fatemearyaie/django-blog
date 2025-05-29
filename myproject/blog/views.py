9fb965c3ee55de28009e3dc6ee6bd0370fd8b801
fatemearyaie <fatemearyaie9@gmail.com>
2025-05-29 21:51:04 +0330

    remove hard coding

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -43,11 +43,12 @@ class CategoryList(ListView):
     paginate_by = 4
     template_name = 'blog/category_list.html'
     def get_queryset(self):
+        global category
         slug = self.kwargs.get('slug')
         category = get_object_or_404(category.objects.CategoryStatus(), slug=slug)
         return category.articles.published()
     def get_context_data(self, **kwargs):
         slug = self.kwargs.get('slug')
         context = super().get_context_data(**kwargs)
-        context['category'] = get_object_or_404(category.objects.CategoryStatus(), slug=slug)
+        context['category'] = category
         return context
