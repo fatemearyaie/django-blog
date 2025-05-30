46fd594acaaea1def30c18c3ef440c429a65f3cb
fatemearyaie <fatemearyaie9@gmail.com>
2025-05-30 12:13:07 +0330

    collect static

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+SPECIFICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
