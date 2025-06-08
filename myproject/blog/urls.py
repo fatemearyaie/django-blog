707660d4ae3b3dd639ad71b2e9e1e65329b8c393
fatemearyaie <fatemearyaie9@gmail.com>
2025-06-08 18:16:50 +0330

    remove unnessecary urls

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('category/<slug:slug>/page/<int:page>/', CategoryList.as_view(), name='category'),
     path('author/<slug:username>/page/<int:page>/', AuthorList.as_view(), name='author'),
     path('account/login/', LoginView.as_view(template_name = 'registeration/login.html'),name='login'),
-    path('accounts/profile/',paneladmin, name='profile'),
-    path('account/list/', ArticleListAdmin.as_view(), name='articlelist'),
+    path('accounts/profile/',ArticleListAdmin.as_view(), name='profile'),
     path('account/article/create/', ArticleCreate.as_view(), name='create')
 ]
