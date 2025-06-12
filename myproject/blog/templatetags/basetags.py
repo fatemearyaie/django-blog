13ae56a0e57a5403c6883b28bc9f463b13097520
fatemearyaie <fatemearyaie9@gmail.com>
2025-06-12 19:34:59 +0330

    use basetags for active menue

--- myproject/blog/templatetags/basetags.py
+++ myproject/blog/templatetags/basetags.py
@@ -12,3 +12,12 @@ def category_navbar():
     return {
         "category" : category.objects.filter(status=True)
     }
+
+@register.inclusion_tag("registeration/partials/link.html")
+def link(request, link_name, content):
+    return {
+        "request" : request,
+        "content" : content,
+        "link_name" : link_name,
+        "link" : "blog:{}".format(link_name),
+    }
